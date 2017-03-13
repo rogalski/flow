87e7249093eb5e441b5c5b08c1207c8311f306b1
Łukasz Rogalski <rogalski.91@gmail.com>
2017-03-13 21:51:55 +0100

    very simple embedding now work, but tests still are not automatic

--- flow/graph.py
+++ flow/graph.py
@@ -16,58 +16,64 @@ class Node:
                                           type(self.statement).__name__)
 
 
-class StartNode(Node):
+class NonStatementNode(Node):
     def __init__(self, statement=None):
-        super(StartNode, self).__init__(statement)
+        super(NonStatementNode, self).__init__(statement)
 
 
-class StopNode(Node):
-    def __init__(self, statement=None):
-        super(StopNode, self).__init__(statement)
+class StartNode(NonStatementNode):
+    pass
+
+
+class StopNode(NonStatementNode):
+    pass
 
 
 class DecisionNode(Node):
     pass
 
 
-class MergeNode(Node):
+class MergeNode(NonStatementNode):
     pass
 
+
 @functools.singledispatch
-def flow_graph_from_ast(ast_node):
+def flow_graph_from_ast(ast_node, expand=False):
     graph = FlowGraph()
-    statement = graph.add_statement(ast_node)
+    statement = graph.add_statement_node(ast_node)
     graph.add_edge(graph.start_node, statement)
     graph.add_edge(statement, graph.end_node)
     return graph
 
 
 @flow_graph_from_ast.register(list)
-def _(ast_list):
+def _(ast_list, expand=False):
     graph = FlowGraph()
-    current_exec_point = graph.start_node
-    if not ast_list:
-        graph.add_edge(graph.start_node, graph.end_node)
-    else:
-        for ast_node in ast_list:
-            graph.embed(flow_graph_from_ast(ast_node),
-                        start_node=current_exec_point, end_node=graph.end_node)
-            current_exec_point = graph.end_node
+    graph.add_edge(graph.start_node, graph.end_node)
+    for ast_node in ast_list:
+        graph.insert(flow_graph_from_ast(ast_node), graph.end_node)
     return graph
 
 
 @flow_graph_from_ast.register(ast.Module)
 @flow_graph_from_ast.register(ast.FunctionDef)
 @flow_graph_from_ast.register(ast.AsyncFunctionDef)
-def _(ast_root):
-    return flow_graph_from_ast(ast_root.body)
+def _(ast_root, expand=False):
+    if expand:
+        return flow_graph_from_ast(ast_root.body)
+    else:
+        graph = FlowGraph()
+        statement = graph.add_statement_node(ast_root)
+        graph.add_edge(graph.start_node, statement)
+        graph.add_edge(statement, graph.end_node)
+        return graph
 
 
 @flow_graph_from_ast.register(ast.If)
-def _(ast_if):
+def _(ast_if, expand=False):
     graph = FlowGraph()
-    decision_if = graph.add_node(DecisionNode(ast_if))
-    merge_if = graph.add_node(MergeNode(ast_if))
+    decision_if = graph.add_decision_node(ast_if)
+    merge_if = graph.add_merge_node()
     graph.add_edge(graph.start_node, decision_if)
     graph.add_edge(merge_if, graph.end_node)
     graph.embed(flow_graph_from_ast(ast_if.body),
@@ -85,40 +91,103 @@ class FlowGraph:
     def __init__(self):
         self.start_node = StartNode()
         self.end_node = StopNode()
-        self.statements = []
+        self.user_nodes = set()
         self.edges = collections.defaultdict(set)
 
     @property
     def statement_count(self):
-        return len(self.statements)
+        return sum(1 for n in self.user_nodes if n.statement is not None)
 
-    def add_statement(self, ast_node):
-        statement = Node(ast_node)
-        self.statements.append(statement)
-        return statement
+    @classmethod
+    def from_ast(cls, ast_root):
+        graph = flow_graph_from_ast(ast_root, expand=True)
+        graph.reduce_merge_nodes()
+        return graph
 
-    def add_node(self, node):
-        self.statements.append(node)
-        return node
+    def add_statement_node(self, ast_stmt_node):
+        statement_node = Node(ast_stmt_node)
+        self.user_nodes.add(statement_node)
+        return statement_node
 
-    def embed(self, other_graph, start_node, end_node):
-        self.statements.extend(other_graph.statements)
-        for from_edge in other_graph.edges:
-            to_nodes = other_graph.edges[from_edge]
-            if other_graph.end_node in to_nodes:
-                to_nodes = (to_nodes | {end_node}) - {other_graph.end_node}
-            if other_graph.start_node in to_nodes:
-                to_nodes = (to_nodes | {start_node}) - {other_graph.start_node}
-            if from_edge == other_graph.start_node:
-                from_edge = start_node
-            self.edges[from_edge].update(to_nodes)
+    def add_decision_node(self, ast_test_node):
+        decision_node = DecisionNode(ast_test_node)
+        self.user_nodes.add(decision_node)
+        return decision_node
+
+    def add_merge_node(self):
+        merge_node = MergeNode()
+        self.user_nodes.add(merge_node)
+        return merge_node
 
     def add_edge(self, from_node, to_node):
         self.edges[from_node].add(to_node)
 
-    @classmethod
-    def from_ast(cls, ast_root):
-        return flow_graph_from_ast(ast_root)
+    def embed(self, other_graph, start_node, end_node):
+        """Given two embed points, embed other flow graph in this graph"""
+        self.user_nodes.update(other_graph.user_nodes)
+
+        for edge_start in other_graph.edges:
+            edge_stops = {s for s in other_graph.edges[edge_start]}
+
+            if other_graph.end_node in edge_stops:
+                edge_stops.remove(other_graph.end_node)
+                edge_stops.add(end_node)
+            if other_graph.start_node in edge_stops:
+                edge_stops.remove(other_graph.start_node)
+                edge_stops.add(start_node)
+
+            if edge_start == other_graph.start_node:
+                edge_start = start_node
+
+            self.edges[edge_start].update(edge_stops)
+
+    def insert(self, other_graph, insert_node):
+        """Given single embed point, embed other flow graph in this graph"""
+        start_node, end_node = self.insert_fake_merge_nodes(insert_node)
+        self.embed(other_graph, start_node, end_node)
+
+    def insert_fake_merge_nodes(self, insert_node):
+        start_node = self.add_merge_node()
+        end_node = self.add_merge_node()
+        for from_node in list(self.edges):
+            to_nodes = self.edges[from_node]
+            if insert_node in to_nodes:
+                to_nodes.remove(insert_node)
+                to_nodes.add(start_node)
+            if insert_node is from_node:
+                self.edges[end_node] = self.edges.pop(insert_node)
+        self.edges[end_node].add(insert_node)
+        return start_node, end_node
+
+    def reduce_merge_nodes(self):
+        """If there are merge nodes with single incoming edge and single outgoing edge,
+        it's removed and respective nodes are connected with simple edge"""
+
+        for node in list(self.user_nodes):
+            if not isinstance(node, MergeNode):
+                continue
+            to_nodes = self.edges[node]
+            if len(to_nodes) != 1:
+                continue
+            from_nodes = [checked_node for checked_node in self.edges
+                          if node in self.edges[checked_node]]
+            if len(from_nodes) != 1:
+                continue
+
+            from_node = next(iter(from_nodes))
+            to_node = next(iter(to_nodes))
+
+            # remove node itself
+            self.user_nodes.remove(node)
+
+            # remove old connections
+            self.edges[node].remove(to_node)
+            self.edges[from_node].remove(node)
+
+            # add new connection
+            self.edges[from_node].add(to_node)
+
+            return self.reduce_merge_nodes()  # recursion here is quite ugly
 
     def to_graphviz(self):
         def add_node(dot, node, **kwargs):
@@ -129,12 +198,28 @@ class FlowGraph:
 
         dot = graphviz.Digraph()
         add_node(dot, self.start_node, shape='circle', label='')
-        add_node(dot, self.end_node, shape='circle', style='filled', label='')
-        for s in self.statements:
-            n = add_node(dot, node=s, label=str(s), shape='diamond' if isinstance(s, DecisionNode) else 'rect')
+        add_node(dot, self.end_node, shape='doublecircle', label='')
+        for node in self.user_nodes:
+            add_node(dot, node=node,
+                     label=self.get_graphviz_label(node),
+                     shape=self.get_graphviz_shape(node))
 
         for from_node in self.edges:
             for to_node in self.edges[from_node]:
                 add_edge(dot, from_node, to_node)
 
         return dot
+
+    @staticmethod
+    def get_graphviz_shape(node):
+        if isinstance(node, (DecisionNode, MergeNode)):
+            return 'diamond'
+        return 'rect'
+
+    @staticmethod
+    def get_graphviz_label(node):
+        if node.statement is None:
+            return ''
+        elif isinstance(node, DecisionNode):
+            return str(node.statement.test)
+        return str(node.statement)
--- flow/tests/test_graph.py
+++ flow/tests/test_graph.py
@@ -5,43 +5,86 @@ import textwrap
 from flow import FlowGraph
 
 
+def ast_from_text(code, filename="<unknown>"):
+    return ast.parse(textwrap.dedent(code), filename)
+
+
+def test_func_name():
+    return sys._getframe(1).f_code.co_name
+
+
 def test_from_empty_module():
-    ast_root = ast.parse("")
+    ast_root = ast_from_text("")
     graph = FlowGraph.from_ast(ast_root)
-    assert graph.statement_count == 0
-    graph.to_graphviz().render(sys._getframe().f_code.co_name + '.gv')
+    graph.to_graphviz().render('{}.gv'.format(test_func_name()))
 
 
 def test_from_single_statement_module():
-    ast_root = ast.parse("A = 1")
+    ast_root = ast_from_text("A = 1")
     graph = FlowGraph.from_ast(ast_root)
     assert graph.statement_count == 1
-    graph.to_graphviz().render(sys._getframe().f_code.co_name + '.gv')
+    graph.to_graphviz().render('{}.gv'.format(test_func_name()))
 
 
 def test_from_two_statement_module():
-    ast_root = ast.parse("A = 1; B = 2")
+    ast_root = ast_from_text("A = 1; B = 2")
     graph = FlowGraph.from_ast(ast_root)
     assert graph.statement_count == 2
-    graph.to_graphviz().render(sys._getframe().f_code.co_name + '.gv')
+    graph.to_graphviz().render('{}.gv'.format(test_func_name()))
 
 
-def test_simple_conditional():
-    ast_root = ast.parse(textwrap.dedent("""
-    if a is not None:
-        print(a.b)
-    else:
-        print(None)
-    """))
+def test_if_conditional():
+    ast_root = ast_from_text("""
+    if 1:
+        print(1)
+    """)
     graph = FlowGraph.from_ast(ast_root)
-    assert graph.statement_count == 4
-    graph.to_graphviz().render(sys._getframe().f_code.co_name + '.gv')
+    assert graph.statement_count == 2  # 1x in if, 1x in if body
+    graph.to_graphviz().render('{}.gv'.format(test_func_name()))
+
+
+def test_if_else_conditional():
+    ast_root = ast_from_text("""
+    if 1:
+        print(1)
+    else:
+        print(2)
+    """)
+    graph = FlowGraph.from_ast(ast_root)
+    assert graph.statement_count == 3  # 1x if stmt, 2x in if bodies
+    graph.to_graphviz().render('{}.gv'.format(test_func_name()))
+
+
+def test_if_elif_conditional():
+    ast_root = ast_from_text("""
+    if 1:
+        print(1)
+    elif 2:
+        print(2)
+    """)
+    graph = FlowGraph.from_ast(ast_root)
+    assert graph.statement_count == 4  # 2x if stmt, 2x in if bodies
+    graph.to_graphviz().render('{}.gv'.format(test_func_name()))
+
+
+def test_if_elif_else_conditional():
+    ast_root = ast_from_text("""
+    if 1:
+        print(1)
+    elif 2:
+        print(2)
+    else:
+        print(3)
+    """)
+    graph = FlowGraph.from_ast(ast_root)
+    assert graph.statement_count == 5  # 2x if stmt, 3x in if bodies
+    graph.to_graphviz().render('{}.gv'.format(test_func_name()))
 
 
 def test_self_graph():
     import flow.graph
     with open(flow.graph.__file__) as fd:
         module = fd.read()
-    ast_root = ast.parse(module, flow.graph.__name__)
+    ast_root = ast_from_text(module, flow.graph.__name__)
     graph = FlowGraph.from_ast(ast_root)
     graph.to_graphviz().render(sys._getframe().f_code.co_name + '.gv')
